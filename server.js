09cab8f608b1f49145016d3ceda0944771db28dc
Deji9000 <deji2109@gmail.com>
2018-09-10 12:42:45 -0700

    search building

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const stories = require('./routes/api/stories');
 
 const app = express();
 
+//use routes
+app.use('/api/stories', stories);
 
 
 //BodyParser MiddleWare
@@ -25,12 +27,10 @@ if(process.env.NODE_ENV === 'production')
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-//use routes
-app.use('/api/stories', stories);
 
 const port = process.env.PORT || 5000;
 
